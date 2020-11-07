5128ad380c652d0116ff6930ff58ee3f4b8d275c
duyquangg <nguyenduyquanggg@gmail.com>
2020-11-07 21:26:58 +0700

    add font icon ios

--- android/app/src/main/java/com/qrcode/MainApplication.java
+++ android/app/src/main/java/com/qrcode/MainApplication.java
@@ -1,5 +1,7 @@
 package com.qrcode;
 
+import com.oblador.vectoricons.VectorIconsPackage;
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -25,7 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          return packages;
+          // return packages;
+          return Arrays.<ReactPackage>asList(
+            new MainReactPackage()
+            , new VectorIconsPackage()
+          );
         }
 
         @Override
