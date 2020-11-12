11a33701744c497f22ed05809a0a4a88c06ce1c4
duyquangg <nguyenduyquanggg@gmail.com>
2020-11-12 16:31:05 +0700

    u c

--- src/reducers/global/globalInitialState.js
+++ src/reducers/global/globalInitialState.js
@@ -8,37 +8,33 @@ var InitialState = Record({
         token:'',
         username: '',
         fullName : '',
-        phone : '',
+
+
         email: '',
-        avatar:'',
-        verified: [],
-        employeeID: '',
-        roleList: [],
-        isExport: ''
+        password: '',
     })),
 
-    deviceInfo : {
-        deviceID : null,
-        deviceModel: null,
-        tokenID : null,
-        tokenOs : null,
-        tokenRegistered: false,
-        appVersion: null
-    },
+    // deviceInfo : {
+    //     deviceID : null,
+    //     deviceModel: null,
+    //     tokenID : null,
+    //     tokenOs : null,
+    //     tokenRegistered: false,
+    //     appVersion: null
+    // },
 
-    appInfo : {
-        version : null,
-        platform: null
-    },
+    // appInfo : {
+    //     version : null,
+    //     platform: null
+    // },
 
-    loggedIn : false,
-    dataNoti: null,
-    isLoggin : false,
-    savedPassword: false,
-    scene : {},
-    prevScene: {},
-    mountTabCv: 1,
-    imageUploading: false,
+    // loggedIn : false,
+    // isLoggin : false,
+    // savedPassword: false,
+    // scene : {},
+    // prevScene: {},
+    // mountTabCv: 1,
+    // imageUploading: false,
 });
 
 export default InitialState;
