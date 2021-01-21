3b8b9fa738116ba029a87226fd31853750930b30
duyquangg <nguyenduyquanggg@gmail.com>
2021-01-22 01:04:21 +0700

    u c

--- src/components/profile/CreateQr.js
+++ src/components/profile/CreateQr.js
@@ -374,3 +374,257 @@ const styles = StyleSheet.create({
     },
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CreateQr);
+
+
+
+// import React, { Component } from 'react';
+// import {
+//     View,
+//     Text,
+//     StyleSheet,
+// } from 'react-native';
+// import { Actions } from 'react-native-router-flux';
+// import FontAwesome from 'react-native-vector-icons/FontAwesome';
+// import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+// import { QRCode, Canvas } from 'easyqrcode-react-native';
+
+// import { bindActionCreators } from 'redux';
+// import { connect } from 'react-redux';
+// import { Map } from 'immutable';
+
+// import Toast from "../toast/Toast";
+// import CommonHeader from '../header/CommonHeader';
+// import gui from '../../lib/gui';
+
+// import * as globalActions from '../../reducers/global/globalActions';
+
+// const actions = [
+//     globalActions
+// ];
+
+// function mapStateToProps(state) {
+//     return {
+//         ...state
+//     };
+// }
+
+// function mapDispatchToProps(dispatch) {
+//     const creators = Map()
+//         .merge(...actions)
+//         .filter(value => typeof value === 'function')
+//         .toObject();
+
+//     return {
+//         actions: bindActionCreators(creators, dispatch),
+//         dispatch
+//     };
+// }
+// class CreateQr extends Component {
+//     constructor(props) {
+//         super(props);
+
+
+//         this.state = {
+
+//         };
+//     }
+//     generateQRCode = (canvas) => {
+//         if (canvas !== null) {
+//             // QRCode options
+//             var options = {
+//                 // ====== Basic
+//                 text: "CHECKIN AT PTIT",
+//                 width: 256,
+//                 height: 256,
+//                 colorDark: "#000000",
+//                 colorLight: "#ffffff",
+//                 correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
+
+//                 // ====== dotScale
+//                 dotScale: 1, // For body block, must be greater than 0, less than or equal to 1. default is 1
+
+//                 dotScaleTiming: 1, // Dafault for timing block , must be greater than 0, less than or equal to 1. default is 1
+//                 dotScaleTiming_H: undefined, // For horizontal timing block, must be greater than 0, less than or equal to 1. default is 1
+//                 dotScaleTiming_V: undefined, // For vertical timing block, must be greater than 0, less than or equal to 1. default is 1
+
+//                 dotScaleA: 1, // Dafault for alignment block, must be greater than 0, less than or equal to 1. default is 1
+//                 dotScaleAO: undefined, // For alignment outer block, must be greater than 0, less than or equal to 1. default is 1
+//                 dotScaleAI: undefined, // For alignment inner block, must be greater than 0, less than or equal to 1. default is 1
+
+//                 // ====== Quiet Zone
+//                 quietZone: 0,
+//                 quietZoneColor: "rgba(0,0,0,0)",
+
+//                 // ====== Logo
+//                 logo: "https://portal.ptit.edu.vn/wp-content/uploads/2016/04/ptit-logo.png",  //  support: Static Image Resources, Network Images, Base64 Uri Data Images
+//                 logoWidth: 80, // width. default is automatic width
+//                 logoHeight: 80, // height. default is automatic height
+//                 logoBackgroundColor: '#fffff', // Logo backgroud color, Invalid when `logBgTransparent` is true; default is '#ffffff'
+//                 logoBackgroundTransparent: false, // Whether use transparent image, default is false
+
+//                 // ====== Backgroud Image
+//                 backgroundImage: '', // support: Static Image Resources, Network Images, Base64 Uri Data Images
+//                 backgroundImageAlpha: 1, // Background image transparency, value between 0 and 1. default is 1. 
+//                 autoColor: false, // Automatic color adjustment(for data block)
+//                 autoColorDark: "rgba(0, 0, 0, .6)", // Automatic color: dark CSS color
+//                 autoColorLight: "rgba(255, 255, 255, .7)", // Automatic color: light CSS color
+
+//                 // ====== Colorful
+//                 // === Posotion Pattern(Eye) Color
+//                 PO: '#e1622f', // Global Posotion Outer color. if not set, the defaut is `colorDark`
+//                 PI: '#aa5b71', // Global Posotion Inner color. if not set, the defaut is `colorDark`
+//                 PO_TL: '', // Posotion Outer color - Top Left 
+//                 PI_TL: '', // Posotion Inner color - Top Left 
+//                 PO_TR: '', // Posotion Outer color - Top Right 
+//                 PI_TR: '', // Posotion Inner color - Top Right 
+//                 PO_BL: '', // Posotion Outer color - Bottom Left 
+//                 PI_BL: '', // Posotion Inner color - Bottom Left 
+//                 // === Alignment Color
+//                 AO: '', // Alignment Outer. if not set, the defaut is `colorDark`
+//                 AI: '', // Alignment Inner. if not set, the defaut is `colorDark`
+//                 // === Timing Pattern Color
+//                 timing: '#e1622f', // Global Timing color. if not set, the defaut is `colorDark`
+//                 timing_H: '', // Horizontal timing color
+//                 timing_V: '', // Vertical timing color
+
+//                 // ====== Title
+//                 title: 'Test QR Code', // content 
+//                 titleFont: "bold 18px Arial", //font. default is "bold 16px Arial"
+//                 titleColor: "#004284", // color. default is "#000"
+//                 titleBackgroundColor: "#fff", // background color. default is "#fff"
+//                 titleHeight: 70, // height, including subTitle. default is 0
+//                 titleTop: 25, // draws y coordinates. default is 30
+
+//                 // ====== SubTitle
+//                 subTitle: 'CHECKIN AT PTIT', // content
+//                 subTitleFont: "14px Arial", // font. default is "14px Arial"
+//                 subTitleColor: "#004284", // color. default is "4F4F4F"
+//                 subTitleTop: 45, // draws y coordinates. default is 0
+
+//                 // ===== Event Handler
+//                 onRenderingStart: undefined,
+//                 onRenderingEnd: undefined,
+
+//                 // ===== Versions
+//                 version: 2, // The symbol versions of QR Code range from Version 1 to Version 40. default 0 means automatically choose the closest version based on the text length.
+
+//                 // ===== Binary(hex) data mode
+//                 binary: false // Whether it is binary mode, default is text mode. 
+//             }
+//             // Create QRCode Object
+//             var qrCode = new QRCode(canvas, options);
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <View style={styles.container}>
+//                 {this._renderHeader()}
+//                 {this._renderBody()}
+//                 <Toast
+//                     ref="toastTop"
+//                     position='top'
+//                     positionValue={70}
+//                     fadeInDuration={1000}
+//                     fadeOutDuration={2000}
+//                     opacity={0.85}
+//                     textStyle={{ color: 'white', fontWeight: '600', textAlign: 'center' }}
+//                 />
+//             </View>
+//         );
+//     }
+//     _renderHeader() {
+//         return (
+//             <CommonHeader
+//                 title={"Tạo mã QR"}
+//                 leftContent={<FontAwesome name={'chevron-left'} color={'#fff'} size={20} />}
+//                 onPressLeft={() => Actions.pop()}
+//             />
+//         );
+//     }
+//     _renderBody() {
+//         let { value, selectedError, selectedMode, check, version } = this.state;
+//         return (
+//             <KeyboardAwareScrollView
+//                 style={{ flex: 1, width: '100%' }}
+//                 keyboardShouldPersistTaps="handled"
+//             >
+//                 <Canvas ref={this.generateQRCode} />
+
+//             </KeyboardAwareScrollView>
+//         );
+//     }
+
+// }
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//     },
+//     body: {
+//         flex: 1,
+//     },
+//     viewRow: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//         alignItems: 'center',
+//         width: gui.screenWidth - 32,
+//         marginLeft: 16,
+//         marginTop: 20,
+//     },
+//     input: {
+//         height: 46,
+//         width: gui.screenWidth - 32,
+//         marginLeft: 16,
+//         borderRadius: 5,
+//         overflow: 'hidden',
+//         color: '#000',
+//         borderWidth: 1,
+//         borderColor: '#34626c',
+//         backgroundColor: 'white',
+//         marginTop: 20,
+//         marginBottom: 10,
+//         paddingHorizontal: 16
+//     },
+//     viewRow: {
+//         flexDirection: 'row',
+//         alignItems: 'center',
+//         marginLeft: 16,
+//         width: gui.screenWidth - 32,
+//     },
+//     viewChooseModal: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//         alignItems: 'center',
+//         width: 40,
+//         height: 30,
+//         marginLeft: 10,
+//         padding: 5,
+//         borderRadius: 5,
+//         borderWidth: 1,
+//         borderColor: '#34626c',
+//     },
+//     viewButton: {
+//         height: 40,
+//         width: gui.screenWidth - 32,
+//         marginLeft: 16,
+//         marginTop: 15,
+//         borderRadius: 8,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#34626c'
+//     },
+//     footerText: {
+//         color: '#fff',
+//         fontSize: 16,
+//         fontWeight: 'bold',
+//     },
+//     viewQR: {
+//         marginTop: 50,
+//         justifyContent: 'center',
+//         alignItems: 'center'
+//     },
+// });
+// export default connect(mapStateToProps, mapDispatchToProps)(CreateQr);
+
+
